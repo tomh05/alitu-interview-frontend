38201b2320ae2e462a9d85866dcc17b2a059e824
Branislav Kacer <branislav.kacer@gmail.com>
2021-04-15 20:42:45 +0200

    Move mock api response to a const

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,17 +1,19 @@
 import { rest } from 'msw'
 import { v4 as uuid } from 'uuid'
 
+const episodes = [
+  {
+    id: uuid(),
+    name: 'First episode',
+    description: 'Launching our podcast'
+  }
+]
+
 export const handlers = [
   rest.get('/api/episodes', (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json([
-        {
-          id: uuid(),
-          name: 'First episode',
-          description: 'Launching our podcast'
-        }
-      ]),
+      ctx.json(episodes),
     )
   }),
 ]
