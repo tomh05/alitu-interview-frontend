30e0b8d46062f2e78bdda8b2d8e56ac46574d37e
Sean <scjwalsh@gmail.com>
2021-04-15 14:58:46 +0300

    Created useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch<Res extends unknown>(url: string, options?: RequestInit) {
+  const [response, setResponse] = useState<Res>();
+  const [error, setError] = useState<unknown>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      try {
+        const res = await fetch(url, options);
+        const json = await res.json();
+        setResponse(json);
+        setIsLoading(false)
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, [options, url]);
+
+  return { response, error, isLoading };
+};
